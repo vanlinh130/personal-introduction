b4cd8334f9b9337bdab12debebb7ec44f679acb4
vanlinh1330 <levanlinh1330@gamil.com>
2022-05-15 19:34:10 +0700

    personal-introduction

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/**       typeing animation        */
+var tuped = new Typed('.typing', {
+    strings: [
+        'Web designer',
+        'Web Developer',
+        'Graphic Designer',
+        'YouTube'
+    ],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+}) 
